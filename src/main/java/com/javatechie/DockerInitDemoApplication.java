2eef32a3bd3b5198b5b8d7e9a0f2145205bbcc4d
juancvilla <juancvilla@gmail.com>
2024-11-20 19:42:43 +0000

    some files 2 changed

--- src/main/java/com/javatechie/DockerInitDemoApplication.java
+++ src/main/java/com/javatechie/DockerInitDemoApplication.java
@@ -1,6 +1,3 @@
-package com.javatechie;
-
-import com.javatechie.dto.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
